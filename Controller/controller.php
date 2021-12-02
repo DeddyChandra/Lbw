01eae4f3f76f0c6fa5daf46b87e0f5c3469d9a7d
rajasacikal <75054325+rajasacikal@users.noreply.github.com>
2021-12-02 10:45:54 +0700

    Top By Fans
    + Menambahkan controller topbyfans
    + Menambah view topbyfans.php
    + Menambah javascript topbyfans.js

--- Controller/controller.php
+++ Controller/controller.php
@@ -1,18 +1,26 @@
-<?php 
-   require 'controller/services/view.php';
+<?php
+require 'controller/services/view.php';
 
-   class Controller{
-      
-      public function view_search(){
-         return view::createView('search.php',[]);
-      }
+class Controller
+{
 
-      public function view_compare(){
-         return view::createView('compare.php',[]);
-      }
-
-      public function view_brands(){
-         return view::createView('search.php',[]);
-      }
+   public function view_search()
+   {
+      return view::createView('search.php', []);
    }
-?>
+
+   public function view_compare()
+   {
+      return view::createView('compare.php', []);
+   }
+
+   public function view_brands()
+   {
+      return view::createView('search.php', []);
+   }
+
+   public function view_topbyfans()
+   {
+      return view::createView('topbyfans.php', []);
+   }
+}
--- router.php
+++ router.php
@@ -1,30 +1,34 @@
 <?php
-	$baseURL = '/lbw';
-	$url = $_SERVER['REDIRECT_URL'];
-	if ($_SERVER["REQUEST_METHOD"] == "GET") {
-		switch($url){
-			case $baseURL. '/search';
-				require_once 'Controller/controller.php';
-				$controller = new Controller();
-				echo $controller->view_search();
-				break;
-			case $baseURL. '/compare';
-				require_once 'Controller/controller.php';
-				$controller = new Controller();
-				echo $controller->view_compare();
-				break;
-			case $baseURL. '/brands';
-				require_once 'Controller/controller.php';
-				$controller = new Controller();
-				echo $controller->view_brands();
-				break;
-			default:
-				echo "Page not Found 404";
-				break;
-		}
+$baseURL = '/lbw';
+$url = $_SERVER['REDIRECT_URL'];
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+	switch ($url) {
+		case $baseURL . '/search';
+			require_once 'Controller/controller.php';
+			$controller = new Controller();
+			echo $controller->view_search();
+			break;
+		case $baseURL . '/compare';
+			require_once 'Controller/controller.php';
+			$controller = new Controller();
+			echo $controller->view_compare();
+			break;
+		case $baseURL . '/brands';
+			require_once 'Controller/controller.php';
+			$controller = new Controller();
+			echo $controller->view_brands();
+			break;
+		case $baseURL . '/topbyfans';
+			require_once 'Controller/controller.php';
+			$controller = new Controller();
+			echo $controller->view_topbyfans();
+			break;
+		default:
+			echo "Page not Found 404";
+			break;
 	}
-	if($_SERVER["REQUEST_METHOD"] == "POST"){
-		switch($url){
-		}
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	switch ($url) {
 	}
-?>
+}
