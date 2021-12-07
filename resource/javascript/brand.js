c779fdb5859293a406d8ba318a8cdbc6cd2be3f2
Michael Sotaronggal Manurung <6181801027@student.unpar.ac.id>
2021-12-07 15:44:42 +0700

    Parsing JSON into 3 collumn div

--- resource/javascript/brand.js
+++ resource/javascript/brand.js
@@ -5,11 +5,14 @@ function getApiInformation(){
         
         let responseJson = response.data;
         let temp = "";
+        let jsonRes = "";
         const phonesContainer = document.getElementById('js-container');
         for(let i = 0; i < responseJson.length; i++){
             temp = responseJson[i];
-            phonesContainer.innerHTML = '<li class="list-group-item">' + temp.brand_name + '</li>';
+            // jsonRes += '<li class="list-group-item">' + temp.brand_name + '</li>';
+            jsonRes += '<div class="col text-center">'+ temp.brand_name +'</div>'
         }
+        phonesContainer.innerHTML = jsonRes;
     });
 }
 
