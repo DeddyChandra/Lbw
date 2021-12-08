ef809544195e8e8a7c9e3ad2ced67734b29d4077
Xilthz156 <el.jason156@gmail.com>
2021-12-08 21:19:23 +0700

    Merged Main with Latest

---
+++ View/latest.php
@@ -0,0 +1,44 @@
+<script src="resource/javascript/latest.js" defer></script>
+
+<nav aria-label="breadcrumb">
+   <ol class="breadcrumb">
+      <li class="breadcrumb-item" aria-current="page">Latest</li>
+   </ol>
+</nav>
+
+<div class="container">
+   <div class="row mt-5">
+      <div class="col-md">
+         <div class="input-group mb-3">
+            <input type="text" class="form-control input-keyword" placeholder="Search phone by name">
+         </div>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md compareBadges">
+      </div>
+   </div>
+   <div class="row phones-container"></div>
+</div>
+
+<table id="latestTab"></table>
+
+<!-- Modal -->
+<div class="modal fade" id="phoneDetailModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+   <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+      <div class="modal-content phone-detail-modal">
+      </div>
+   </div>
+</div>
+
+<button type="button" id="openCompareButton" class="btn btn-success bottom-right-button invisible">Compare 
+   <!-- <span class="badge badge-danger" id="totalCompareBadge">0</span> -->
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="compareDetailModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+   <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+      <div class="modal-content phone-detail-modal">
+      </div>
+   </div>
+</div>
