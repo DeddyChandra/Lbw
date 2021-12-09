a421a2c852578878ea38b925c0050e4e0c561bd6
Rajasa Cikal <rajasacikalmaulana@gmail.com>
2021-12-09 08:53:46 +0700

    Top By Fans
    + topbyfans.php
    + topbyfans.js

--- View/topbyfans.php
+++ View/topbyfans.php
@@ -7,30 +7,15 @@
 </nav>
 
 <div class="container">
-    <div class="topByFansDiv"></div>
-    <div class="row">
-        <div class="col-md compareBadges">
-        </div>
-    </div>
-    <div class="row phones-container"></div>
-</div>
-
-<!-- Modal -->
-<div class="modal fade" id="phoneDetailModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-        <div class="modal-content phone-detail-modal">
-        </div>
-    </div>
-</div>
-
-<button type="button" id="openCompareButton" class="btn btn-success bottom-right-button invisible">Compare -->
-    <span class="badge badge-danger" id="totalCompareBadge">0</span>
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="compareDetailModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-        <div class="modal-content phone-detail-modal">
-        </div>
-    </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Phone Name</th>
+                <th scope="col">Voted By</th>
+            </tr>
+        </thead>
+        <tbody class="topByFansDiv">
+        </tbody>
+    </table>
 </div>
