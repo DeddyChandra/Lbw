d6af02420609b553d1001705f4b6f8022f0aaff2
Rajasa Cikal <rajasacikalmaulana@gmail.com>
2021-12-09 08:45:27 +0700

    Top By Fans

--- View/topbyfans.php
+++ View/topbyfans.php
@@ -1,38 +1,21 @@
 <script src="resource/javascript/topbyfans.js" defer></script>
 
+<nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+    <ol class="breadcrumb m-3">
+        <li class="breadcrumb-item active">Top by fans</li>
+    </ol>
+</nav>
+
 <div class="container">
-    <h2>Top 10 By Fans</h2>
-    <!-- <div class="row mt-5">
-        <div class="col-md">
-            <div class="input-group mb-3">
-                <input type="text" class="form-control input-keyword" placeholder="Search phone by name">
-            </div>
-        </div>
-    </div> -->
-    <div class="topByFansDiv"></div>
-    <div class="row">
-        <div class="col-md compareBadges">
-        </div>
-    </div>
-    <div class="row phones-container"></div>
-</div>
-
-<!-- Modal -->
-<div class="modal fade" id="phoneDetailModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-        <div class="modal-content phone-detail-modal">
-        </div>
-    </div>
-</div>
-
-<button type="button" id="openCompareButton" class="btn btn-success bottom-right-button invisible">Compare -->
-    <span class="badge badge-danger" id="totalCompareBadge">0</span>
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="compareDetailModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-        <div class="modal-content phone-detail-modal">
-        </div>
-    </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Phone Name</th>
+                <th scope="col">Voted By</th>
+            </tr>
+        </thead>
+        <tbody class="topByFansDiv">
+        </tbody>
+    </table>
 </div>
