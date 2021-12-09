31a1ef95eb8100420f258fd817ea8fd759dc5868
quadratnp <quadratnp@gmail.com>
2021-12-09 11:22:26 +0700

    Update controller.php

--- Controller/controller.php
+++ Controller/controller.php
@@ -14,21 +14,18 @@ class Controller
       return view::createView('compare.php', []);
    }
 
-   public function view_brands()
-   {
-      return view::createView('search.php', []);
-   }
-
    public function view_topbyfans()
    {
       return view::createView('topbyfans.php', []);
    }
-  
-   public function view_brands(){
-     return view::createView('brands.php',[]);
+
+   public function view_brands()
+   {
+      return view::createView('brands.php', []);
    }
-  
-    public function view_topByInterest(){
-       return view::createView('topByInterest.php',[]);
-    }
+
+   public function view_topByInterest()
+   {
+      return view::createView('topByInterest.php', []);
+   }
 }
